71bc406d616bb5c1db2ea82ce1310d71be7c1e6f
Pedro Pereira <devpereirapedro@gmail.com>
2025-05-21 20:51:41 -0300

    fix: user middleware

--- src/middlewares/user-webhook.ts
+++ src/middlewares/user-webhook.ts
@@ -1,45 +1,45 @@
-import type { Handler } from 'elysia';
-import { Webhook } from 'svix';
+import type { Handler } from "elysia";
+import { Webhook } from "svix";
 
 export const userWebhookMidleware: Handler = async (c) => {
-	const CLERK_SIGNING_SECRET = Bun.env['CLERK_SIGNING_SECRET'];
+  const CLERK_SIGNING_SECRET = Bun.env["CLERK_WEBHOOK_SIGNING_SECRET"];
 
-	if (!CLERK_SIGNING_SECRET) {
-		throw new Error('CLERK_SIGNING_SECRET is not set');
-	}
+  if (!CLERK_SIGNING_SECRET) {
+    throw new Error("CLERK_SIGNING_SECRET is not set");
+  }
 
-	const wh = new Webhook(CLERK_SIGNING_SECRET);
+  const wh = new Webhook(CLERK_SIGNING_SECRET);
 
-	const { headers } = c.request;
+  const { headers } = c.request;
 
-	const svix_id = headers.get('svix-id');
-	const svix_timestamp = headers.get('svix-timestamp');
-	const svix_signature = headers.get('svix-signature');
+  const svix_id = headers.get("svix-id");
+  const svix_timestamp = headers.get("svix-timestamp");
+  const svix_signature = headers.get("svix-signature");
 
-	if (!svix_id || !svix_timestamp || !svix_signature) {
-		throw new Error('Error: Missing svix headers');
-	}
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    throw new Error("Error: Missing svix headers");
+  }
 
-	console.log('Headers:', { svix_id, svix_timestamp, svix_signature });
+  console.log("Headers:", { svix_id, svix_timestamp, svix_signature });
 
-	// Attempt to verify the incoming webhook
-	// If successful, the payload will be available from 'evt'
-	// If verification fails, error out and return error code
-	try {
-		const rawBody = JSON.stringify(c.body);
-		console.log('Body:', rawBody);
+  // Attempt to verify the incoming webhook
+  // If successful, the payload will be available from 'evt'
+  // If verification fails, error out and return error code
+  try {
+    const rawBody = JSON.stringify(c.body);
+    console.log("Body:", rawBody);
 
-		const clerkEvent = wh.verify(rawBody, {
-			'svix-id': svix_id as string,
-			'svix-timestamp': svix_timestamp as string,
-			'svix-signature': svix_signature as string,
-		});
+    const clerkEvent = wh.verify(rawBody, {
+      "svix-id": svix_id as string,
+      "svix-timestamp": svix_timestamp as string,
+      "svix-signature": svix_signature as string,
+    });
 
-		const store = c.store as { clerkEvent: unknown };
-		store.clerkEvent = clerkEvent;
-	} catch (err) {
-		console.log('Error: Could not verify webhook:', err);
+    const store = c.store as { clerkEvent: unknown };
+    store.clerkEvent = clerkEvent;
+  } catch (err) {
+    console.log("Error: Could not verify webhook:", err);
 
-		throw new Error('Error: Could not verify webhook');
-	}
+    throw new Error("Error: Could not verify webhook");
+  }
 };
